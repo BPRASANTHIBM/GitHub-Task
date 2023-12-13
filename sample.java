33e0f0fcdab84f8d0356290c9f48cfbf5ee21ede
Vinolisha <153608805+Vinolisha@users.noreply.github.com>
2023-12-13 12:39:28 +0530

    Revert "partha pull the new features"

--- sample.java
+++ sample.java
@@ -5,7 +5,6 @@ public static void main (String args[]){
 System.out.println("hello World");
 System.out.println("hello sample");
 System.out.println("hello Git workspace");
-  System.out.println("Error");
   
 }
 }
